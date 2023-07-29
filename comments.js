155d4c15ce2ef83640e3bf7f6e981a1f5df45c88
Harish Gupta <124888295+String-slicer@users.noreply.github.com>
2023-07-29 11:31:12 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+//create new server
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose'); //mongo connection
+var bodyParser = require('body-parser'); //parses information from POST
+var methodOverride = require('method-override'); //used to manipulate POST
+
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.use(methodOverride(function(req, res){
+    if(req.body && typeof req.body === 'object' && '_method' in req.body){
+        //look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
